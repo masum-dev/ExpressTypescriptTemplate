8dc8c21331fb183362e779dcf003386706a038e0
Mahbub Alam Masum <mail.masum.dev@gmail.com>
2026-05-04 17:32:18 +0600

    Environment Variables Configured

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import  express  from 'express';
+import {serverConfig} from './config/index.ts';
+
+const app = express();
+
+
+app.get('/ping', (req, res) => {
+    res.send("Pong")
+});
+
+app.listen(serverConfig.PORT, ()=> {
+    console.log(`Server running on PORT: ${serverConfig.PORT}`);
+})
